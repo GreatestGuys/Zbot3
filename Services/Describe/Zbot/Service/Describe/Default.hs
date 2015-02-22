953744f2fc716dc8d1f22b8f5e9ab04d0e930343
Will Coster <willcoster@gmail.com>
2015-02-21 22:21:32 -0800

    Grab meta tag descriptions.

--- Services/Describe/Zbot/Service/Describe/Default.hs
+++ Services/Describe/Zbot/Service/Describe/Default.hs
@@ -4,12 +4,34 @@ module Zbot.Service.Describe.Default (
 
 import Zbot.Service.Describe
 
+import Control.Applicative
 import Text.HTML.Scalpel
 
 import qualified Data.Text as T
 
 describeDefault :: Describer
-describeDefault url= Just $ scrapeURLAsSearchEngine url title
+describeDefault url= Just $ scrapeURLAsSearchEngine url description
+
+description :: Scraper T.Text T.Text
+description = metaDescription
+            <|> metaFacebookDescription
+            <|> metaTwitterDescription
+            <|> title
+
+scrapeMetaTag :: T.Text -> Scraper T.Text T.Text
+scrapeMetaTag name = do
+    content <- attr "content"
+            $ ("meta" :: T.Text) @: [("name" :: T.Text) @= name]
+    if T.null content then empty else return content
+
+metaDescription :: Scraper T.Text T.Text
+metaDescription = scrapeMetaTag "description"
+
+metaFacebookDescription :: Scraper T.Text T.Text
+metaFacebookDescription = scrapeMetaTag "og:description"
+
+metaTwitterDescription :: Scraper T.Text T.Text
+metaTwitterDescription = scrapeMetaTag "twitter:description"
 
 title :: Scraper T.Text T.Text
 title = text $ ("title" :: T.Text)
