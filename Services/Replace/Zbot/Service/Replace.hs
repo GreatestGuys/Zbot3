c5b6c26bdd642bb6446a37f20c5d90f3a155eb06
Will Coster <willcoster@gmail.com>
2015-03-28 14:03:09 -0700

    Add replace service (closes #14)

---
+++ Services/Replace/Zbot/Service/Replace.hs
@@ -0,0 +1,61 @@
+module Zbot.Service.Replace (
+    replace
+)   where
+
+import Zbot.Core.Bot
+import Zbot.Core.Irc
+import Zbot.Core.Service
+import Zbot.Extras.UnitService
+import Zbot.Service.History
+
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Class (lift)
+import Data.Monoid
+
+import qualified Data.Text as T
+
+
+-- | A service that will replace a substring of a users previous message.
+replace :: (MonadIO m, Bot m) => Handle History -> Service m ()
+replace history = unitService "Zbot.Service.Replace" handler
+    where
+        handler (Shout channel nick msg) = lift $ do
+            maybeLine <- findLastMessage channel nick
+            handleMessage (shout channel) maybeLine msg
+        handler _                        = return ()
+
+        handleMessage reply (Just line) msg
+            | (_:from:to:"g":_) <- split msg = reply $ replaceAll from to line
+            | (_:from:to:_)     <- split msg = reply $ replaceOne from to line
+        handleMessage _ _ _                  = return ()
+
+        findLastMessage channel nick = foldHistoryBackward history match Nothing
+            where
+                match _ (Shout channel' nick' msg) Nothing
+                    | channel == channel'
+                      && nick == nick'
+                      && not ("s/" `T.isPrefixOf` msg) = Just msg
+                match _ _ acc = acc
+
+-- | Split a string on all non-escaped back slashes.
+split :: T.Text -> [T.Text]
+split s = splitHelper "" s
+    where
+        splitHelper acc s
+            | T.null s               = [acc]
+            | "\\/" `T.isPrefixOf` s = splitHelper (acc <> "\\/") (T.drop 2 s)
+            | "/" `T.isPrefixOf` s   = acc : splitHelper "" (T.drop 1 s)
+            | otherwise              = splitHelper
+                                            (acc <> T.take 1 s)
+                                            (T.drop 1 s)
+
+replaceAll :: T.Text -> T.Text -> T.Text -> T.Text
+replaceAll from to t
+    | T.null from = t
+    | otherwise   = T.replace from to t
+
+replaceOne :: T.Text -> T.Text -> T.Text -> T.Text
+replaceOne from to t
+    | T.null from || T.null t = t
+    | from `T.isPrefixOf` t   = to <> T.drop (T.length from) t
+    | otherwise               = (T.take 1 t) <> replaceOne from to (T.drop 1 t)
