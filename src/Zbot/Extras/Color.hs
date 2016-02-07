8128e55968b018dbba6cb2f0c5d691f61d51c593
Will Coster <willcoster@gmail.com>
2016-02-07 15:22:34 -0800

    Make Zbot.Extras.Color use mIRC colors
    Previously the Color module was using ANSI color escapes for color
    coding output. This worked great when testing the bot locally, but
    failed miserably when using real IRC clients. The fix is to use mIRC
    color encoding.
    
    Issue #38

--- src/Zbot/Extras/Color.hs
+++ src/Zbot/Extras/Color.hs
@@ -3,9 +3,9 @@ module Zbot.Extras.Color (
 ,   ColorText
 
 ,   bg
-,   blink
 ,   bold
 ,   fg
+,   italic
 ,   strikeThrough
 ,   underline
 
@@ -18,66 +18,90 @@ import Data.String (IsString, fromString)
 import qualified Data.Text as T
 
 
-data Color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
+data Color
+    = White
+    | Black
+    | Blue
+    | Green
+    | Red
+    | Brown
+    | Purple
+    | Orange
+    | Yellow
+    | LightGreen
+    | Cyan
+    | LightCyan
+    | LightBlue
+    | Pink
+    | Grey
+    | LightGrey
 
 data Style
-    = Foreground Color
-    | Background Color
-    | Bold
-    | Blink
+    = Bold
+    | Italic
     | Underline
     | StrikeThrough
 
-data ColorText = ColorText [Style] T.Text
+data ColorText = ColorText (Maybe Color) (Maybe Color) [Style] T.Text
 
 instance IsString ColorText where
-    fromString str = ColorText [] (T.pack str)
-
+    fromString str = ColorText Nothing Nothing [] (T.pack str)
 
 codeFromColor :: Color -> T.Text
-codeFromColor Black   = "0"
-codeFromColor Red     = "1"
-codeFromColor Green   = "2"
-codeFromColor Yellow  = "3"
-codeFromColor Blue    = "4"
-codeFromColor Magenta = "5"
-codeFromColor Cyan    = "6"
-codeFromColor White   = "7"
+codeFromColor White      = "00"
+codeFromColor Black      = "01"
+codeFromColor Blue       = "02"
+codeFromColor Green      = "03"
+codeFromColor Red        = "04"
+codeFromColor Brown      = "05"
+codeFromColor Purple     = "06"
+codeFromColor Orange     = "07"
+codeFromColor Yellow     = "08"
+codeFromColor LightGreen = "09"
+codeFromColor Cyan       = "10"
+codeFromColor LightCyan  = "11"
+codeFromColor LightBlue  = "12"
+codeFromColor Pink       = "13"
+codeFromColor Grey       = "14"
+codeFromColor LightGrey  = "15"
+
+codeFromColorPair :: Maybe Color -> Maybe Color -> T.Text
+codeFromColorPair Nothing  Nothing  = ""
+codeFromColorPair (Just f) Nothing  = "\x03" `T.append` codeFromColor f
+codeFromColorPair (Just f) (Just b) = T.concat [ "\x03", codeFromColor f
+                                               , ",",    codeFromColor b ]
+codeFromColorPair Nothing  (Just b) = T.concat [ "\x03", codeFromColor Black
+                                               , ",",    codeFromColor b ]
 
 codeFromStyle :: Style -> T.Text
-codeFromStyle (Background color) = "4" `T.append` codeFromColor color
-codeFromStyle (Foreground color) = "3" `T.append` codeFromColor color
-codeFromStyle Blink              = "5"
-codeFromStyle Bold               = "1"
-codeFromStyle StrikeThrough      = "9"
-codeFromStyle Underline          = "4"
+codeFromStyle Bold               = "\x02"
+codeFromStyle StrikeThrough      = "\x13"
+codeFromStyle Underline          = "\x1f"
+codeFromStyle Italic             = "\x09"
 
 -- | Colorize and concatenate a list of `ColorText`s.
 colorize :: [ColorText] -> T.Text
 colorize = T.concat . map go
-  where go (ColorText styles text) = T.concat [
-                "\x1b[", T.intercalate ";" (map codeFromStyle styles) ,"m"
-            ,   text
-            ,   "\x1b[0m"
-            ]
+    where
+        go (ColorText Nothing Nothing []     text) =  text
+        go (ColorText f       b       styles text) =  T.concat
+                                                   $  codeFromColorPair f b
+                                                   :  map codeFromStyle styles
+                                                   ++ [text, "\x0f"]
 
 colorText :: T.Text -> ColorText
-colorText = ColorText []
+colorText = ColorText Nothing Nothing []
 
 addStyle :: Style -> ColorText -> ColorText
-addStyle style (ColorText styles text) = ColorText (style : styles) text
+addStyle style (ColorText f b styles text) = ColorText f b (style : styles) text
 
 -- | Set the ColorText's background color.
 bg :: Color -> ColorText -> ColorText
-bg = addStyle . Background
+bg c (ColorText f _ s t) = ColorText f (Just c) s t
 
 -- | Set the ColorText's foreground color.
 fg :: Color -> ColorText -> ColorText
-fg = addStyle . Foreground
-
--- | Make the ColorText blink.
-blink :: ColorText -> ColorText
-blink = addStyle Blink
+fg c (ColorText _ b s t) = ColorText (Just c) b s t
 
 -- | Make the ColorText bold.
 bold :: ColorText -> ColorText
@@ -90,3 +114,7 @@ strikeThrough = addStyle StrikeThrough
 -- | Make the ColorText underlined.
 underline :: ColorText -> ColorText
 underline = addStyle StrikeThrough
+
+-- | Make the ColorText underlined.
+italic :: ColorText -> ColorText
+italic = addStyle Italic
