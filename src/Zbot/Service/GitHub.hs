fcaa111eb08b7c9dac7704139b61b12981384672
Jesse Frankley <jesse@climate.com>
2017-01-14 21:16:33 -0800

    added \!close, resolves issue #44

--- src/Zbot/Service/GitHub.hs
+++ src/Zbot/Service/GitHub.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Zbot.Service.GitHub (
     GitHub
 ,   github
+,   reportIssue
 ) where
 
 import Zbot.Core.Bot
 import Zbot.Core.Service
-import Zbot.Extras.Command
+import Zbot.Extras.Message
 import Zbot.Extras.Serialize
 
 import Control.Lens ((&), (.~), (^?))
@@ -37,7 +39,7 @@ github = Service {
     ,   serialize   = serializeShow
     ,   deserialize = deserializeRead
     ,   name        = "Zbot.Service.GitHub"
-    ,   process     = onCommand "!report" handleCommand
+    ,   process     = onMessage handler
     ,   helpSpec    = Just HelpSpec {
             helpAliases = ["!report"]
         ,   helpMessage = [
@@ -50,28 +52,66 @@ github = Service {
         }
     }
 
-handleCommand :: (Catch.MonadCatch m, MonadIO m, Bot m)
-              => Reply m -> T.Text -> MonadService GitHub m ()
-handleCommand reply issue =   gets unGitHub
-                          >>= lift . maybe reportNotConfigured reportIssueSafe
-    where
-        issueEndpoint = "https://api.github.com/repos/GreatestGuys/Zbot3/issues"
+handler :: (Catch.MonadCatch m, MonadIO m, Bot m)
+        => Reply m
+        -> T.Text
+        -> MonadService GitHub m ()
+handler reply msg
+        | ("!report":issue)    <- args = githubHandler reportIssue reply $ T.concat issue
+        | ("!close":number:[]) <- args = githubHandler closeIssue reply number
+        | otherwise                    = return ()
+        where
+            args = T.words msg
 
+type GitHubAction = forall m. (MonadIO m) => Reply m -> T.Text -> GitHubAccessToken -> m ()
+
+reportIssue :: GitHubAction
+reportIssue reply issue accessToken = do
+        result <- liftIO $ postWith options issueEndpoint payload
+        reply $ toIssueUrl result
+        where
+            options = defaults & param "access_token" .~ [accessToken]
+
+            payload = object ["title" .= issue]
+
+            issueEndpoint = "https://api.github.com/repos/GreatestGuys/Zbot3/issues"
+
+            -- The result is a JSON object, one of the fields is "url" which is the
+            -- URL of the newly created issue.
+            toIssueUrl result = fromMaybe errorMsg
+                              $ result ^? responseBody . key "html_url" . _String
+
+            errorMsg = "Encountered an error while filing the issue."
+
+closeIssue :: GitHubAction
+closeIssue reply number accessToken = do
+        result <- liftIO $ postWith options issueEndpoint payload
+        reply $ toIssueUrl result
+        where
+            options = defaults & param "access_token" .~ [accessToken]
+
+            payload = object ["state" .= number]
+
+            issueEndpoint =  "https://api.github.com/repos/GreatestGuys/Zbot3/issues/"
+                          ++ T.unpack number
+
+            -- The result is a JSON object, one of the fields is "url" which is the
+            -- URL of the newly created issue.
+            toIssueUrl result = fromMaybe errorMsg
+                              $ result ^? responseBody . key "html_url" . _String
+
+            errorMsg = "Encountered an error while closing the issue."
+
+
+githubHandler :: (Catch.MonadCatch m, MonadIO m, Bot m)
+              => GitHubAction -> Reply m -> T.Text -> MonadService GitHub m ()
+githubHandler h reply issue =   gets unGitHub
+                            >>= lift . maybe reportNotConfigured reportIssueSafe
+    where
         reportNotConfigured = reply "GitHub service is not configured."
 
-        reportIssueSafe = flip Catch.catch errorHandler . reportIssue
+        reportIssueSafe = flip Catch.catch errorHandler . (h reply issue)
 
-        reportIssue accessToken = do
-            let options = defaults & param "access_token" .~ [accessToken]
-            let payload = object ["title" .= issue]
-            result <- liftIO $ postWith options issueEndpoint payload
-            reply $ toIssueUrl result
-
-        -- The result is a JSON object, one of the fields is "url" which is the
-        -- URL of the newly created issue.
-        toIssueUrl result = fromMaybe errorMsg
-                          $ result ^? responseBody . key "html_url" . _String
-
-        errorMsg = "Encountered an error while filing the issue."
+        errorMsg = "Encountered an error while accessing GitHub api."
 
         errorHandler (_ :: Catch.SomeException) = reply errorMsg
