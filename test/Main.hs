b6df6fa6838c96c288353bf7caa67d0c378742cf
jesse <number.dot.ten@gmail.com>
2018-11-05 16:06:37 -0800

    dnd-ified roll service

--- test/Main.hs
+++ test/Main.hs
@@ -3,12 +3,14 @@ module Main where
 
 import Zbot.Tests.Dude
 import Zbot.Tests.Reputation
+import Zbot.Tests.Roll
 
 import Test.Tasty
 
 
 main :: IO ()
 main = defaultMain $ testGroup "ZBot Tests" [
-    dudeTests,
-    reputationTests
+    dudeTests
+  , reputationTests
+  , rollTests
   ]
