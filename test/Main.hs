477d7a833fda3b3046787d1eb7196b3749d4d2dc
Will Coster <willcoster@gmail.com>
2019-10-18 21:44:50 -0700

    Add tests for onion component
    Fixes #147

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Zbot.Tests.Dude
+import Zbot.Tests.Onion
 import Zbot.Tests.Replace
 import Zbot.Tests.Reputation
 import Zbot.Tests.Roll
@@ -14,6 +15,7 @@ import Test.Tasty
 main :: IO ()
 main = defaultMain $ testGroup "ZBot Tests" [
     dudeTests
+  , onionTests
   , replaceTests
   , reputationTests
   , rollTests
