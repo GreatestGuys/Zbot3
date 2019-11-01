250f0009dfc57f6c5daf2b0e7bc6af51731586f6
Will Coster <willcoster@gmail.com>
2019-10-31 18:57:40 -0700

    Revert "Fix YouTube parsing"
    This reverts commit 302a65a88bdc07a810de77d1275bb8ea8d8c0f87.

--- test/Zbot/TestCase.hs
+++ test/Zbot/TestCase.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Zbot.TestCase (
   mockBotTestCase
-, mockBotTestCaseWithAssert
-, outputMessage
 , replyOutput
 ) where
 
@@ -20,24 +18,14 @@ import qualified Data.Text as T
 -- | Creates a test case that executes a series of events in a mock bot and
 -- asserts that the output matches that given.
 mockBotTestCase :: String -> MockBot () -> [Event] -> [Output] -> TestTree
-mockBotTestCase description botInit events expected =
-    mockBotTestCaseWithAssert description botInit events (@?= expected)
-
--- | Creates a test case that executes a series of events in a mock bot and
--- performs a custom assertion on the output.
-mockBotTestCaseWithAssert :: String -> MockBot () -> [Event] -> ([Output] -> Assertion) -> TestTree
-mockBotTestCaseWithAssert description botInit events assertion = testCase description $
+mockBotTestCase description botInit events expected = testCase description $
   withSystemTempDirectory "zbot-test" $ \tempDir -> do
     -- Drop the initial handshake since it is common among all tests.
     actual <- drop 3 <$> evalMockBot tempDir botInit events
-    assertion actual
+    actual @?= expected
 
 -- | Creates an Output value that corresponds to Zbot sending a message to a
 -- channel.
 replyOutput :: T.Text -> T.Text -> Output
 replyOutput channel message =
     Output BestEffort (Message Nothing "PRIVMSG" [channel] (Just message))
-
--- | Returns the message component of an Output value.
-outputMessage :: Output -> T.Text
-outputMessage (Output _ (Message Nothing "PRIVMSG" [_] (Just msg))) = msg
